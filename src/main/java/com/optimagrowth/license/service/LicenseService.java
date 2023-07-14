5505131ae85c944d003a3a714a57962584f8976b
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-07-14 11:15:15 -0300

    Localization & HATEOAS

--- src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -1,12 +1,18 @@
 package com.optimagrowth.license.service;
 
 import com.optimagrowth.license.model.License;
+import java.util.Locale;
 import java.util.Random;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LicenseService {
-  public License getLicense(String licenseId, String organizationId){
+
+  @Autowired MessageSource messages;
+
+  public License getLicense(String licenseId, String organizationId) {
     License license = new License();
     license.setId(new Random().nextInt(1000));
     license.setLicenseId(licenseId);
@@ -16,25 +22,32 @@ public class LicenseService {
     license.setLicenseType("full");
     return license;
   }
-  public String createLicense(License license, String organizationId){
-    String responseMessage = null;
-    if(license != null) {
-      license.setOrganizationId(organizationId);
-      responseMessage = String.format("This is the post and the object is: %s", license.toString());
-    }
-    return responseMessage;
-  }
-  public String updateLicense(License license, String organizationId){
+
+  public String createLicense(License license, String organizationId, Locale locale) {
     String responseMessage = null;
     if (license != null) {
       license.setOrganizationId(organizationId);
-      responseMessage = String.format("This is the put and the object is: %s", license.toString());
+      responseMessage =
+          String.format(messages.getMessage("license.create.message", null, locale), license);
     }
     return responseMessage;
   }
-  public String deleteLicense(String licenseId, String organizationId){
+
+  public String updateLicense(License license, String organizationId, Locale locale) {
     String responseMessage = null;
-    responseMessage = String.format("Deleting license with id %s for the organization %s",licenseId, organizationId);
+    if (license != null) {
+      license.setOrganizationId(organizationId);
+      responseMessage =
+          String.format(
+              messages.getMessage("license.update.message", null, locale), license.toString());
+    }
+    return responseMessage;
+  }
+
+  public String deleteLicense(String licenseId, String organizationId, Locale locale) {
+    String responseMessage = null;
+    responseMessage =
+        String.format(messages.getMessage("license.delete.message", null, locale), licenseId, organizationId);
     return responseMessage;
   }
 }
